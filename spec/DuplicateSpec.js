4d65b2857f78f3c51dd0a1025f0b1682e62dcdde
SD13 <SD13@mitt.ca>
2018-05-22 08:41:38 -0500

    Question 2

--- spec/DuplicateSpec.js
+++ spec/DuplicateSpec.js
@@ -11,4 +11,20 @@ describe(positiveSum, function(){
     it("should return zero if all numbers are negative", function(){
         expect([-5, -6, -8]).toBe(0);
     })
-})
+});
+
+
+describe(isDivisible, function(){
+    it("should return true if the number is divisible by both the divisors", function(){
+        expect(isDivisible(8, 2, 4)).toBe(true);
+    })
+    it("should return false if number is smaller than both of the divisors", function(){
+        expect(isDivisible(2, 4, 8)).toBe(false);
+    })
+    it("should return false if number is divisile by only one of teh divisors", function(){
+        expect(isDivisible(6, 2, 4)).toBe(false);
+    })
+    it("should return false if number is not divisible by any of the numbers", function(){
+        expect(isDivisible(7, 2, 4)).toBe(false);
+    })
+});
--- src/Duplicates.js
+++ src/Duplicates.js
@@ -9,3 +9,5 @@ const positiveSum = (numbers) => {
       
     return sum;
   }
+
+  const isDivisible = (number, divisorA, divisorB) => number % divisorB === 0 && number % divisorA === 0
